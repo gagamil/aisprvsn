ab2f7a500f24353d5d862dcf9b3851b22b215f9f
Gabriel Gamil <gamil.gabriel@gmail.com>
2022-09-22 14:08:32 +0300

    Add Model to hold the data - initial version

--- stocks/admin.py
+++ stocks/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from import_export import resources
+from import_export.admin import ImportExportModelAdmin
 
-# Register your models here.
+from .models import StockValue
+
+
+class StockValueResource(resources.ModelResource):
+    class Meta:
+        model = StockValue
+        fields = ('id', 'ticker', 'date', 'value_open', 'value_high', 'value_low', 'value_close', 'value_adjclose', 'volume')
+
+
+class StockValueAdmin(ImportExportModelAdmin):
+    resource_class = StockValueResource
+    search_fields = (
+        "ticker",
+    )
+
+admin.site.register(StockValue, StockValueAdmin)
--- stocks/models.py
+++ stocks/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
-# Create your models here.
+
+STCK_SYMBOL__GOOG = 'GOOG'
+STCK_SYMBOL__PLTR = 'PLTR'
+STCK_SYMBOL__TWTR = 'TWTR'
+STCK_SYMBOL__SAP = 'SAP'
+STCK_SYMBOL__CHOICES = (
+                        (STCK_SYMBOL__GOOG, 'Alphabet Inc.'),
+                        (STCK_SYMBOL__PLTR, 'Palantir Technologies Inc.'),
+                        (STCK_SYMBOL__TWTR, 'Twitter, Inc.'),
+                        (STCK_SYMBOL__SAP, 'SAP SE')
+                       )
+
+
+class StockValue(models.Model):
+    ticker = models.CharField(choices=STCK_SYMBOL__CHOICES, max_length=5)
+    date = models.DateField()
+    value_open = models.DecimalField(max_digits=13, decimal_places=6, blank=True, null=True)
+    value_high = models.DecimalField(max_digits=13, decimal_places=6, blank=True, null=True)
+    value_low = models.DecimalField(max_digits=13, decimal_places=6, blank=True, null=True)
+    value_close = models.DecimalField(max_digits=13, decimal_places=6, blank=True, null=True)
+    value_adjclose = models.DecimalField(max_digits=13, decimal_places=6, blank=True, null=True)
+    volume = models.PositiveIntegerField(blank=True, null=True)
+
+    def __str__(self):
+        return f'{self.ticker}, {self.date}'
