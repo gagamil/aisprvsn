657479a194332db3918a503a77e69a4daa5d6959
Gabriel Gamil <gamil.gabriel@gmail.com>
2022-09-24 13:35:56 +0300

    Use dtclss in signal payload

---
+++ common/data.py
@@ -0,0 +1,17 @@
+from dataclasses import dataclass
+from datetime import date
+from typing import List
+from dataclasses_json import dataclass_json
+
+
+@dataclass
+class StockData:
+    id: int
+    ticker: str
+    date: date
+    value_open: float
+    value_high: float
+    value_low: float
+    value_close: float
+    value_adjclose: float
+    volume: int
---
+++ common/signals.py
@@ -0,0 +1,11 @@
+import logging
+import django.dispatch
+
+from common.data import StockData
+
+logger = logging.getLogger(__name__)
+
+stockdata_import_done = django.dispatch.Signal()
+def sig_send__stockdata_import_done(*, sender, sender_pk: int, stockdata: StockData) -> None:
+    logger.info(f'SIG "sig_send__stockdata_import_done" will be sent with imported stock data. Import ID: sender_pk {sig_send__stockdata_import_done.id}')
+    stockdata_import_done.send(sender=sender, sender_pk=sender_pk, stockdata=stockdata)
