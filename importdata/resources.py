f2429bdfb0882f42dc70e0654edc0c937e2c189b
Gabriel Gamil <gamil.gabriel@gmail.com>
2022-09-26 09:59:29 +0300

    Use dynamic ticker

--- importdata/resources.py
+++ importdata/resources.py
@@ -4,9 +4,13 @@ from .models import StockValueImportData
 
 
 class StockValueResource(resources.ModelResource):
+    def __init__(self, ticker):
+        super().__init__()
+        self.ticker = ticker
     class Meta:
         model = StockValueImportData
         fields = ('id', 'date', 'value_open', 'value_high', 'value_low', 'value_close', 'value_adjclose', 'volume')
 
     def before_import_row(self, row, row_number=None, **kwargs):
+        row['ticker'] = self.ticker
         print(row, row_number, kwargs)
--- importdata/tests.py
+++ importdata/tests.py
@@ -8,9 +8,10 @@ from .resources import StockValueResource
 
 class ImportTestCase(TestCase):
     def test_data_import_ok(self):
-        import_file = settings.BASE_DIR / 'data' / 'original' / 'GOOG.csv'
+        ticker = 'GOOG'
+        import_file = settings.BASE_DIR / 'data' / 'original' / f'{ticker}.csv'
         with import_file.open('r') as f:
-            resource = StockValueResource()
+            resource = StockValueResource(ticker)
             dataset = tablib.Dataset().load(f)
             result = resource.import_data(dataset, dry_run=True, raise_errors=True)
             # print(result.has_errors())
