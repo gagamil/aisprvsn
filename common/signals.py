fcaf9663f4575646732ba1ff259af005db7b1a2c
Gabriel Gamil <gamil.gabriel@gmail.com>
2022-09-26 09:27:37 +0300

    Quickfix typo

--- common/signals.py
+++ common/signals.py
@@ -7,5 +7,5 @@ logger = logging.getLogger(__name__)
 
 stockdata_import_done = django.dispatch.Signal()
 def sig_send__stockdata_import_done(*, sender, sender_pk: int, stockdata: StockData) -> None:
-    logger.info(f'SIG "sig_send__stockdata_import_done" will be sent with imported stock data. Import ID: sender_pk {sig_send__stockdata_import_done.id}')
+    logger.info(f'SIG "sig_send__stockdata_import_done" will be sent with imported stock data. Import ID: {sender_pk=}')
     stockdata_import_done.send(sender=sender, sender_pk=sender_pk, stockdata=stockdata)
